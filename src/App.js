29e2230a4be7008ae4275032e49e7a71dbd5f38d
Kadu Lisboa <kadulisboa301@icloud.com>
2021-02-03 21:58:44 -0300

    Learned about Props and Components

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { render } from 'react-dom';
+import Header from './components/Header';
+
+export default function App() {
+    return (
+        <Header title="Site do Kadu">
+            <ul>
+                <li>
+                    Teste
+                </li>
+                <li>
+                    Teste
+                </li>
+                <li>
+                    Teste
+                </li>
+                <li>
+                    Teste
+                </li>
+            </ul>
+        </Header>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Header from './Header';
+import App from './App';
 
-render(<Header />, document.getElementById('app'));
+render(
+
+    <App />,
+
+    document.getElementById('app')
+);
